e6bae60e8c8be903bf890fcb68e35a0dc6ca0be5
amir5pro <amirmiftah555@gmail.com>
2024-01-22 14:56:48 +0300

    third commit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -50,7 +50,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
