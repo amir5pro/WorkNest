63c90a37786276859a16d27e8c740244ed8aeb5c
amir5pro <amirmiftah555@gmail.com>
2024-01-23 09:18:18 +0300

    4th commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import Job from "../models/jobModel.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -20,12 +21,11 @@ export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
-
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
 
   if (req.file && updatedUser.avatarPublicId) {
